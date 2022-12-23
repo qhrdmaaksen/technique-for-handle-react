e93119ce1941d3d7335861a078d3a7148321f0ea
vitamin777 <rlaalsdn8@gmail.com>
2022-12-24 01:16:06 +0900

    feat: 목록 더블 클릭시 목록삭제 기능 추가
    -목록이 출려될 li tag 에 onDoubleClick 속성으로 onRemove 함수에 더블 클릭된 name id 를 보내도록함
    -onRemove 함수에서 인수로 id 가 들어오면 해당 id 와 기존의 id 를 비교해 인수로 들어온 아이디를 제외한 name 을 setNames 로 업데이트해줌
    
    -input 에 onKeyPress 속성 정의하여
    onKeyPressHandler 함수에서 키를 누르면 누른 키가 엔터일시 onClickAddListHandler 를 호출해줘서 엔터 눌러도 리스트에 목록 출력되도록함

--- src/etc/mapListSamepleStudy/IterationSampleTest.jsx
+++ src/etc/mapListSamepleStudy/IterationSampleTest.jsx
@@ -2,28 +2,22 @@ import {useState} from "react";
 
 const IterationSampleTest = () => {
 	const [names, setNames] = useState([
-		{
-			id: 1,
-			text: '눈사람',
-		},
+		{id: 1, text: '눈사람'},
 		{
 			id: 2,
-			text: '얼음',
+			text: '얼음'
 		},
 		{
 			id: 3,
-			text: '눈',
+			text: '눈'
 		},
 		{
 			id: 4,
-			text: '바람',
+			text: '바람'
 		},
 	])
 	const [inputText, setInputText] = useState('')
 	const [nextId, setNextId] = useState(5) // 새로운 항목을 추가 시 사용할 id
-
-	const nameList = names.map(name => <li key={name.id}>{name.text}</li>)
-	console.log(nameList)
 
 	const inputOnChangeHandler = (e) => {
 		setInputText(e.target.value)
@@ -39,9 +33,30 @@ const IterationSampleTest = () => {
 		setNextId(nextId + 1) // nextId 값에 1 더해줌
 	}
 
+	/** nameList 함수에서 onRemove 통해 들어온 id 와
+	 * 기존의 names 의 아이디와 비교해 제외하고 나머지를 names 로 업데이트해줌*/
+	const onRemove = (id) => {
+		const nextNames = names.filter(name => name.id !== id)
+		setNames(nextNames)
+	};
+
+	const onKeyPressHandler = (e) => {
+		if (e.key === 'Enter') {
+			onClickAddListHandler()
+		}
+	}
+
+	/** li tag 더블 클릭 시 onRemove 함수 호출*/
+	const nameList = names.map(name => (
+			<li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+				{name.text}
+			</li>
+	))
+	console.log(nameList)
+
 	return (
 			<>
-				<input value={inputText} onChange={inputOnChangeHandler}/>
+				<input value={inputText} onChange={inputOnChangeHandler} onKeyPress={onKeyPressHandler}/>
 				<button onClick={onClickAddListHandler}>추가</button>
 				<ul>{nameList}</ul>
 			</>
