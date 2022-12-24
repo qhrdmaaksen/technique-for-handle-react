ba0aa12fd144bba86813c48f980809c31dde2933
vitamin777 <rlaalsdn8@gmail.com>
2022-12-24 23:57:01 +0900

    chore: 라이프 사이클 & 에러 처리 공부내용 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import React, {Component} from "react";
 import ScrollBox from './etc/scrollSampleStudy/ScrollBox'
 import IterationSampleTest from "./etc/mapListSamepleStudy/IterationSampleTest";
+import LifeCycleSample from "./etc/lifeCycleSampleStudy/LifeCycleSample";
+import ErrorBoundary from "./etc/lifeCycleSampleStudy/ErrorBoundary";
 
 /*function App() {
   return (
@@ -24,11 +26,37 @@ import IterationSampleTest from "./etc/mapListSamepleStudy/IterationSampleTest";
     </div>
   );
 }*/
+
+// 랜덤 색상을 생성함
+// getRandomColor 함수는 state 의 color 값을 랜덤 색상으로 설정/16777215 를 hex 로 표현하면 ffffff 가 되며 해당 코드는 000000 부터 ffffff 값을 반환함
+//버튼을 렌더링하고 누를때 handleClick 메서드가 호출되기 이벤트를 설정했으며 불러온 LifeCycleSample 컴포넌트에 color 값을 props 로 설정함
+function getRandomColor() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
+
 class App extends Component {
+	state = {
+		color: '#000000'
+	}
+	handleClick = () => {
+		this.setState({
+			color: getRandomColor()
+		})
+	}
+
 	render() {
 		return (
-				<IterationSampleTest />
+				<div>
+					<button onClick={this.handleClick}>랜덤 색상</button>
+					<ErrorBoundary>
+						<LifeCycleSample color={this.state.color}/>
+					</ErrorBoundary>
+				</div>
 		)
+		/*map list 출력 테스트
+		return (
+				<IterationSampleTest />
+		)*/
 		/*
 		scroll test
 		return (
