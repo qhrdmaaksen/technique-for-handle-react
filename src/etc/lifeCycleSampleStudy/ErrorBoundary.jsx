165cbe5f59041240e28277e3d68d5d82affc2b91
vitamin777 <rlaalsdn8@gmail.com>
2022-12-24 23:59:14 +0900

    chore: render 오타로 인한 에러 수정 후 커밋

--- src/etc/lifeCycleSampleStudy/ErrorBoundary.jsx
+++ src/etc/lifeCycleSampleStudy/ErrorBoundary.jsx
@@ -6,15 +6,20 @@ class ErrorBoundary extends Component {
 	state = {
 		error: false,
 	}
+
 	componentDidCatch(error, errorInfo) {
 		this.setState({
 			error: true
 		})
 		console.log(error, errorInfo)
 	}
-	redner() {
-		if (this.state.error) return <div>에러가 발생했습니다.</div>
+
+	render() {
+		if (this.state.error) {
+			return <div>에러가 발생했습니다.</div>
+		}
 		return this.props.children
 	}
 }
+
 export default ErrorBoundary;
