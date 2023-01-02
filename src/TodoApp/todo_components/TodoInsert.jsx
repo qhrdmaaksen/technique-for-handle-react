92885d2825627e99a6fd2a6f25d58021d69cad02
vitamin777 <rlaalsdn8@gmail.com>
2023-01-02 18:36:12 +0900

    feat: 스케쥴 입력 후 목록에 추가 기능 구현
    -App.js 에서 onInsert 함수를 TodoInsert component 에 props 로 전달해줬으며 TodoInsert.jsx 에서 전달받은 onInsert 함수를 onSubmit 함수에서 state value 값을 onInsert 함수에 보내고 값을 초기화 해줬으며 useCallback 사용해 onInsert, value 가 변경될 시 함수 재사용토록하였고 onInsert 로 보내진 value 는 다시 App.js 에 onInsert 함수의 text 로 보내져 id 를 갖게되고 setTodos 로 todos 배열에 concat 으로 입력된 스케쥴이 추가되도록했으며 todos 배열이 업데이트됨에따라 TodoList component 에 업데이트된 todos 가 props 로 전달돼 출력토록하였음

--- src/TodoApp/todo_components/TodoInsert.jsx
+++ src/TodoApp/todo_components/TodoInsert.jsx
@@ -1,28 +1,40 @@
-import {MdAdd} from 'react-icons/md';
-import './TodoInsert.scss';
-import {useCallback, useState} from "react";
+import { MdAdd } from "react-icons/md";
+import "./TodoInsert.scss";
+import { useCallback, useState } from "react";
 
-const TodoInsert = () => {
-	const [value, setValue] = useState('')
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState("");
 
-	/** input 입력 값 받기
-	 * 컴포넌트가 리렌더링될때마다 함수를 새로만드는것이아닌
-	 * 한번 함수를 만들고 재사용할 수 있도록 useCallback 사용*/
-	const onChange = useCallback((e) => {
-		setValue(e.target.value)
-	}, [])
+  /** input 입력 값 받기
+   * 컴포넌트가 리렌더링될때마다 함수를 새로만드는것이아닌
+   * 한번 함수를 만들고 재사용할 수 있도록 useCallback 사용*/
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
 
-	return (
-			<form className="TodoInsert">
-				<input
-					placeholder="할 일을 입력하세요."
-					value={value}
-					onChange={onChange}
-				/>
-				<button type="submit">
-					<MdAdd/>
-				</button>
-			</form>
-	)
-}
-export default TodoInsert
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(""); // value 값 초기화
+
+      // submit 이벤트는 브라우저에서 새로고침을 발생시킴
+      // 이를 방지하기 위해 이 함수를 호출함
+      e.preventDefault();
+    },
+    [onInsert, value]
+  );
+
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요."
+        value={value}
+        onChange={onChange}
+      />
+      <button type="submit">
+        <MdAdd />
+      </button>
+    </form>
+  );
+};
+export default TodoInsert;
